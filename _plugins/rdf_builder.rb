d656e6ec42b4a32d095a589d871456365c1ea6a6
Sebastian Skałacki <skalee@gmail.com>
2020-02-23 04:44:19 +0100

    wip Define RDF Builder

---
+++ _plugins/rdf_builder.rb
@@ -0,0 +1,145 @@
+require "rdf"
+require "rdf/turtle"
+require "rdf/vocab"
+
+class RDFBuilder
+  # Shortcut access to vocabularies (no need to prefix with RDF)
+  DC = RDF::Vocab::DC
+  OWL = RDF::OWL
+  RDFS = RDF::RDFS
+  SKOS = RDF::Vocab::SKOS
+  XSD = RDF::XSD
+
+  Profile = RDF::Vocabulary.new("/api/rdf-profile#")
+
+  # This is concept data, which is different than site.data
+  attr_reader :data
+
+  # Supported languages defined for this concept
+  attr_reader :languages
+
+  # Jekyll::Site instance
+  attr_reader :site
+
+  def initialize(concept_hash, site)
+    @data = concept_hash
+    @site = site
+    @languages = site.config["term_languages"] & data.keys
+  end
+
+  def graph
+    @graph or build_graph
+  end
+
+  private
+
+  # Graph subjects
+
+  def concept
+    @concept ||= RDF::URI.new("/concepts/#{term_id}/")
+  end
+
+  def concept_closure
+    concept.join("closure")
+  end
+
+  def concept_linked_data_api
+    concept.join("linked-data-api")
+  end
+
+  # Graph predicates
+
+  def concept_classification
+    concept.join("classification")
+  end
+
+  def concept_status
+    concept.join("status")
+  end
+
+  # Easy data accessors
+
+  def term_id
+    data["termid"]&.to_i
+  end
+
+  def en_data
+    data["eng"]
+  end
+
+  def page_url
+    "/api/concepts/#{term_id}.ttl"
+  end
+
+  # Graph building
+
+  def build_graph
+    @graph = RDF::Graph.new
+
+    add_statement(concept, RDF.type, OWL.Ontology)
+
+    # concept itself
+
+    # is trailing '#' okay?
+    add_statement(concept, OWL.imports, DC.to_uri)
+    add_statement(concept, OWL.imports, Profile.to_uri)
+    add_statement(concept, OWL.imports, SKOS.to_uri)
+
+    # concept:closure
+
+    add_statement(concept_closure, RDF.type, SKOS.Concept)
+    add_statement(concept_closure, DC.source, en_data.dig("authoritative_source", "link"))
+
+    add_statement(concept_closure, Profile.termID, RDF::URI(page_url))
+
+    add_statement(concept_closure, SKOS.inScheme, Profile.GeolexicaConceptScheme)
+    add_statement(concept_closure, RDFS.label, en_data["term"]) # TODO escape filter
+    add_statement(concept_closure, SKOS.notation, term_id)
+
+    add_statement(concept_closure, DC.dateAccepted, en_data["date_accepted"]) # TODO format "%F"
+    add_statement(concept_closure, DC.modified, en_data["date_amended"]) # TODO format "%F"
+    add_statement(concept_closure, concept_status, en_data["entry_status"]) # TODO escape
+    add_statement(concept_closure, concept_classification, en_data["classification"]) # TODO escape
+
+    # concept:closure translatable part
+
+    each_language do |lang, l_data|
+      add_statement(concept_closure, Profile["#{lang}Origin"], Profile[language_data(lang)["lang_en"]])
+      add_statement(concept_closure, SKOS.definition, l_data.dig("definition"), language: lang)
+      add_statement(concept_closure, SKOS.prefLabel, l_data.dig("term"), language: lang) # TODO escape filter
+      add_statement(concept_closure, SKOS.altLabel, l_data.dig("alt"), language: lang) # TODO escape filter
+    end
+
+    # concept:linked-data-api
+
+    add_statement(concept_linked_data_api, RDF.type, DC.MediaTypeOrExtent)
+    add_statement(concept_linked_data_api, SKOS.prefLabel, "linked-data-api")
+
+    graph
+  end
+
+  def each_language
+    languages.each do |lang|
+      yield lang, data[lang]
+    end
+  end
+
+  def add_statement(subject, predicate, object, **options)
+    return if object.nil?
+    add_statement!(subject, predicate, object, **options)
+  end
+
+  def add_statement!(subject, predicate, object, language: nil)
+    if language
+      short_code = language_data(language)["iso-639-1"]
+      object = RDF::Literal.new(object, language: short_code)
+    end
+
+    s = RDF::Statement.new(subject, predicate, object)
+    @graph << s
+  end
+
+  def language_data(long_code)
+    site.data["lang"][long_code]
+  end
+end
---
+++ spec/plugins/rdf_builder_spec.rb
@@ -0,0 +1,51 @@
+require_relative "../../_plugins/rdf_builder"
+
+RSpec.describe RDFBuilder do
+  let(:rdfb) { described_class.new(concept_10_data, site) }
+  let(:concept_10_data) { load_concept_fixture("concept-10.yaml") }
+  let(:site) { fake_site }
+
+  it "initializes with a concept hash and site" do
+    instance = described_class.new(concept_10_data, site)
+    expect(instance).to be_kind_of(described_class)
+    expect(instance.data).to eq(concept_10_data)
+    expect(instance.site).to be(site)
+  end
+
+  specify "#graph returns an RDF Graph instance" do
+    retval = rdfb.graph
+    expect(retval).to be_kind_of(::RDF::Graph)
+  end
+
+  %w[9 10 1336].each do |concept_num|
+    specify "#graph returns an RDF Graph with correct structure for " +
+      "concept #{concept_num}" do
+      expected = RDF::Graph.load(fixture_path("concept-#{concept_num}.ttl"))
+      data = load_concept_fixture("concept-#{concept_num}.yaml")
+      rdfb = described_class.new(data, site)
+
+      expected_dump = expected.dump(:ttl)
+      actual_dump = rdfb.graph.dump(:ttl)
+      # TODO Should be, for easier reviewing:
+      # actual_dump = rdfb.graph.dump(:ntriples)
+      # expected_dump = expected.dump(:ntriples)
+      expect(actual_dump).to eq(expected_dump)
+    end
+  end
+
+  def load_concept_fixture(fixture_name)
+    YAML.load(fixture(fixture_name))
+  end
+
+  def fake_site
+    config_path = File.expand_path("../../_config.yml", __dir__)
+    lang_data_path = File.expand_path("../../_data/lang.yaml", __dir__)
+    config = YAML.load(File.read(config_path))
+    lang_data = YAML.load(File.read(lang_data_path))
+
+    double(
+      config: config,
+      data: {"lang" => lang_data},
+    )
+  end
+end

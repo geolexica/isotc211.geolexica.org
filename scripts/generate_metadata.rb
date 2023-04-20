3b6f6266eb97af2173d842b06060e1fda588d0ba
HassanAkbar <hassanakbar2@gmail.com>
2023-04-20 15:07:46 +0500

    using paneron structure for site generation (#190)
    * using paneron structure for site generation
    
    * updated generate metadata script to use paneron dataset
    
    * fixing hound comments

--- scripts/generate_metadata.rb
+++ scripts/generate_metadata.rb
@@ -1,24 +1,25 @@
 #!/usr/bin/env ruby
 
-require 'yaml'
+require "yaml"
+require "date"
 
 terms = []
-Dir['isotc211-glossary/concepts/*.yaml'].map do |yaml_file|
-  terms << YAML.safe_load(IO.read(yaml_file), permitted_classes: [Time])
+Dir["isotc211-glossary/geolexica/concept/*.yaml"].map do |yaml_file|
+  terms << YAML.safe_load(IO.read(yaml_file), permitted_classes: [Date, Time])
   puts "Processing #{yaml_file}"
 end
 
 term_count = terms.map do |t|
-  t.keys.length - 2
+  t["data"]["localizedConcepts"].count
 end.sum
 
 meta = {
-  'concept_count' => terms.length,
-  'term_count' => term_count,
-  'version' => '20200602'
+  "concept_count" => terms.length,
+  "term_count" => term_count,
+  "version" => "20230420",
 }
 
-File.open('metadata.yaml', 'w') do |file|
+File.open("metadata.yaml", "w") do |file|
   file.write(meta.to_yaml)
 end
 

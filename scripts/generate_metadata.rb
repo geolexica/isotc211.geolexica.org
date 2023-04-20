baf3f75238bbc6e54cacaf681d239b417a6fcf06
HassanAkbar <hassanakbar2@gmail.com>
2023-04-20 15:00:24 +0500

    fixing hound comments

--- scripts/generate_metadata.rb
+++ scripts/generate_metadata.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
-require 'yaml'
-require 'date'
+require "yaml"
+require "date"
 
 terms = []
-Dir['isotc211-glossary/geolexica/concept/*.yaml'].map do |yaml_file|
+Dir["isotc211-glossary/geolexica/concept/*.yaml"].map do |yaml_file|
   terms << YAML.safe_load(IO.read(yaml_file), permitted_classes: [Date, Time])
   puts "Processing #{yaml_file}"
 end
@@ -14,12 +14,12 @@ term_count = terms.map do |t|
 end.sum
 
 meta = {
-  'concept_count' => terms.length,
-  'term_count' => term_count,
-  'version' => '20230420'
+  "concept_count" => terms.length,
+  "term_count" => term_count,
+  "version" => "20230420"
 }
 
-File.open('metadata.yaml', 'w') do |file|
+File.open("metadata.yaml", "w") do |file|
   file.write(meta.to_yaml)
 end
 
